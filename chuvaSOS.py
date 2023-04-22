43eb23c1a10f7fc80068bfa2d2d7157083484382
Antonio Cristovam <antoniocristovam@outlook.com.br>
2023-04-22 19:09:21 -0300

    fix: correcting, error getting value from enum

--- chuvaSOS.py
+++ chuvaSOS.py
@@ -1,11 +1,14 @@
+from data.cityCode import cityCode
 from os.path import exists
 from ydata_profiling import ProfileReport
 from data.raw_data import getting_data
 from data.processed_data import process_data
-from data.cityCode import cityCode
 
-unprocessed_data = getting_data(cityCode.Barra_de_Guabiraba)
+
+unprocessed_data = getting_data(cityCode.Barra_de_Guabiraba.value)
+
 dataframe = process_data(unprocessed_data)
+
 
 profile = ProfileReport(df=dataframe, title='TEST')
 file_existis = exists('chuvaSOS.html')
