52d8462a25f21553c70b1c3982b7950970375645
Antonio Cristovam <antoniocristovam@outlook.com.br>
2023-04-22 18:48:03 -0300

    feat: add enum

--- chuvaSOS.py
+++ chuvaSOS.py
@@ -4,13 +4,14 @@ from ydata_profiling import ProfileReport
 from data.raw_data import getting_data
 from data.processed_data import process_data
 
+from data.countyCode import countyCode
 
-unprocessed_data = getting_data('2611606')
+unprocessed_data = getting_data(countyCode.Barra_de_Guabiraba)
 
 dataframe = process_data(unprocessed_data)
 
 
-profile = ProfileReport(df=dataframe, title = 'TEST')
+profile = ProfileReport(df=dataframe, title='TEST')
 file_existis = exists('chuvaSOS.html')
 
 if file_existis != True:
